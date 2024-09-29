c9e3ccabadc552f8a54d99c7e6612a059e875c85
Pranjal Jain <jainpranjal30112002@gmail.com>
2024-09-29 16:42:54 +0530

    [fix]: auth params added

--- auth.ts
+++ auth.ts
@@ -12,6 +12,11 @@ export const authOptions: NextAuthOptions = {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID ?? "",
             clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+            authorization: {
+                params: {
+                scope: 'openid email profile',
+                },
+            },
         }),
     ],
     secret: process.env.NEXTAUTH_SECRET,
